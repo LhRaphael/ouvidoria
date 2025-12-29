97dda4fcef28e28227a44aa73449887de9718f5c
rafael <rafaelfferreira224@gmail.com>
2025-12-29 12:57:57 -0300

    iniciado funções  de perfil

--- frontend/src/pages/UserProfile.jsx
+++ frontend/src/pages/UserProfile.jsx
@@ -1,8 +1,29 @@
+import { useState } from "react";
 
 function UserProfile() {
+  const [nameSignal, setNameSignal] = useState(false);
+
+
   return (
     <div>
-      <h1>User Profile Page</h1>
+      <h2>Perfil</h2>
+      <p>Detalhes do usuário serão exibidos aqui.</p>
+      <div>
+        <img src="" alt="Imagem de perfil" />
+        <button>Alterar imagem</button>
+      </div>
+      <div>
+        <h2>Nome do usuário</h2>
+        {nameSignal ? (
+          <form>
+            <input type="text" placeholder="Novo nome" />
+            <button>Salvar</button>
+            <button onClick={() => setNameSignal(false)}>Cancelar</button>
+          </form>
+        ) : (
+          <button onClick={() => setNameSignal(true)}>Editar nome</button>
+        )}
+      </div>
     </div>
   );
 }
